7da884b2c6e0ca628f12ee8b5308c2de83e50192
RayaanJIrani <rayaanjirani@gmail.com>
2023-10-31 05:14:20 -0500

    Edited the code so that the flashSlider, cameraToggleButton, and torchToggleButton are inoperable

--- ImageLab/ViewController.swift
+++ ImageLab/ViewController.swift
@@ -29,120 +29,53 @@ class ViewController: UIViewController   {
     @IBOutlet weak var cameraToggleButton: UIButton!
     @IBOutlet weak var bpmLabel: UILabel!
     
-    // MARK: ViewController Hierarchy
+// MARK: ViewController Hierarchy
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = nil
         
-        // setup the OpenCV bridge nose detector, from file
         self.bridge.loadHaarCascade(withFilename: "nose")
         
         self.videoManager = VisionAnalgesic(view: self.cameraView)
         self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.back)
         
-        // create dictionary for face detection
-        // HINT: you need to manipulate these properties for better face detection efficiency
-        let optsDetector = [CIDetectorAccuracy:CIDetectorAccuracyHigh,
-                      CIDetectorNumberOfAngles:11,
-                      CIDetectorTracking:false] as [String : Any]
+        let optsDetector = [CIDetectorAccuracy: CIDetectorAccuracyHigh,
+                            CIDetectorNumberOfAngles: 11,
+                            CIDetectorTracking: false] as [String: Any]
         
-        // setup a face detector in swift
         self.detector = CIDetector(ofType: CIDetectorTypeFace,
-                                  context: self.videoManager.getCIContext(), // perform on the GPU is possible
-            options: (optsDetector as [String : AnyObject]))
+                                   context: self.videoManager.getCIContext(),
+                                   options: (optsDetector as [String: AnyObject]))
         
         self.videoManager.setProcessingBlock(newProcessBlock: self.processImageSwift)
         
-        if !videoManager.isRunning{
+        if !videoManager.isRunning {
             videoManager.start()
         }
         
         startUpdatingBPM()
-    
     }
     
-    // MARK: Process Image Output
-//    func processFace(inputImage:CIImage) -> CIImage{
-//         //detect faces
-//        let f = getFaces(img: inputImage)
-//
-//         //if no faces, just return original image
-//        if f.count == 0 { return inputImage }
-//
-//        var retImage = inputImage
-//
-//        self.bridge.setImage(retImage,
-//                             withBounds: f[0].bounds, // the first face bounds
-//                             andContext: self.videoManager.getCIContext())
-//
-//        self.bridge.processImage()
-//        retImage = self.bridge.getImageComposite() // get back opencv processed part of the image (overlayed on original)
-//
-//        return retImage
-//    }
-    
-    // This shows
     func startUpdatingBPM() {
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
-            let bpm = self.bridge.getBetsPerMinute() //calculates bpm in bridge
+            let bpm = self.bridge.getBetsPerMinute()
             self.bpmLabel.text = "BPM: \(bpm)"
         }
     }
-
     
-    func processImageSwift(inputImage:CIImage) -> CIImage{
+    func processImageSwift(inputImage: CIImage) -> CIImage {
         
-        // detect faces
-//        let f = getFaces(img: inputImage)
-        
-        // if no faces, just return original image
-//        if f.count == 0 { return inputImage }
-        
-//        var retImage = inputImage
-        
-        //-------------------Example 1----------------------------------
-        // if you just want to process on separate queue use this code
-        // this is a NON BLOCKING CALL, but any changes to the image in OpenCV cannot be displayed real time
-        /*
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) { () -> Void in
-            self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
-            self.bridge.processImage()
-        }
-         */
-        
-        //-------------------Example 2----------------------------------
-        // use this code if you are using OpenCV and want to overwrite the displayed image via OpenCV
-        // this is a BLOCKING CALL
-        /*
-        // FOR FLIPPED ASSIGNMENT, YOU MAY BE INTERESTED IN THIS EXAMPLE
-        
-        self.bridge.setImage(retImage, withBounds: retImage.extent, andContext: self.videoManager.getCIContext())
-        self.bridge.processImage()
-        retImage = self.bridge.getImage()
-         */
-        
-        //-------------------Example 3----------------------------------
-        //You can also send in the bounds of the face to ONLY process the face in OpenCV
-        // or any bounds to only process a certain bounding region in OpenCV
-        
-        // Initializer
         var retImage = inputImage
         
-        // Set Current Image
         self.bridge.setImage(inputImage,
                              withBounds: inputImage.extent,
                              andContext: self.videoManager.getCIContext())
         
-        // Process Finger
         let isFingerDetected = self.bridge.processFinger()
         let redValue = self.bridge.getRedValue()
-        
-        
-        // Based On Return Value, Enable / Disable Buttons
+
         DispatchQueue.main.async {
-            self.torchToggleButton.isEnabled = !isFingerDetected
-            self.cameraToggleButton.isEnabled = !isFingerDetected
             if isFingerDetected {
                 self.stageLabel.text = String(format: "Red Value: %.2f", redValue)
             } else {
@@ -150,36 +83,22 @@ class ViewController: UIViewController   {
             }
         }
 
-        // Toggle Flash Depending On Return
-        // Note: Only Change If Not Already Controlled
-        if !isFlashManuallyControlled {
-            if isFingerDetected {
-                _ = self.videoManager.turnOnFlashwithLevel(1.0)
-            }
-            else {
-                self.videoManager.turnOffFlash()
-            }
+        if isFingerDetected {
+            _ = self.videoManager.turnOnFlashwithLevel(1.0)
+        } else {
+            self.videoManager.turnOffFlash()
         }
         
-        // Get Back OpenCV Processed Part Of Image
         retImage = self.bridge.getImageComposite()
         
-        // Return Augmented Image
         return retImage
-        
     }
     
-    // MARK: Setup Face Detection
-    
-    func getFaces(img:CIImage) -> [CIFaceFeature]{
-        // this ungodly mess makes sure the image is the correct orientation
-        let optsFace = [CIDetectorImageOrientation:self.videoManager.ciOrientation]
-        // get Face Features
+    func getFaces(img: CIImage) -> [CIFaceFeature] {
+        let optsFace = [CIDetectorImageOrientation: self.videoManager.ciOrientation]
         return self.detector.features(in: img, options: optsFace) as! [CIFaceFeature]
-        
     }
     
-    // change the type of processing done in OpenCV
     @IBAction func swipeRecognized(_ sender: UISwipeGestureRecognizer) {
         switch sender.direction {
         case .left:
@@ -187,46 +106,27 @@ class ViewController: UIViewController   {
                 self.bridge.processType += 1
             }
         case .right:
-            if self.bridge.processType >= 1{
+            if self.bridge.processType >= 1 {
                 self.bridge.processType -= 1
             }
         default:
             break
-            
         }
-        
         stageLabel.text = "Stage: \(self.bridge.processType)"
-
     }
     
-    // MARK: Convenience Methods For UI Flash, Camera Toggle
     @IBAction func flash(_ sender: AnyObject) {
-        
-         // Toggle Flash. If Overheated, Toggle Will Not Change State
-         if (!self.videoManager.toggleFlash()) {
-             isFlashManuallyControlled.toggle()
-         }
-         
-         // Update Slider Value
-         self.flashSlider.value = isFlashManuallyControlled ? 1.0 : 0.0
-         
+        print("Flash button tapped, but no action is implemented.")
     }
-    
+
     @IBAction func switchCamera(_ sender: AnyObject) {
-        self.videoManager.toggleCameraPosition()
+        print("Camera toggle button tapped, but no action is implemented.")
     }
-    
+
     @IBAction func setFlashLevel(_ sender: UISlider) {
-        if (sender.value > 0.0) {
-            isFlashManuallyControlled = true
-            let val = self.videoManager.turnOnFlashwithLevel(sender.value)
-            if val {
-                print("Flash Return, No Errors")
-            }
-        }
-        else if (sender.value == 0.0) {
-            isFlashManuallyControlled = false
-            self.videoManager.turnOffFlash()
-        }
+        print("Flash slider value changed to \(sender.value), but no action is implemented.")
     }
 }
+
+
+
