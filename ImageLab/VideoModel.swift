230faf15f1714a02b0244c1200293f9ad417fa13
Trevor Dohm <trevordohm@gmail.com>
2023-10-31 18:08:32 -0700

    Highlighting Mouth, Eyes, Face

--- ImageLab/VideoModel.swift
+++ ImageLab/VideoModel.swift
@@ -32,7 +32,7 @@ class VideoModel: NSObject {
                             CIDetectorEyeBlink:true,
                             CIDetectorTracking:false,
                       CIDetectorMinFeatureSize:0.1,
-                     CIDetectorMaxFeatureCount:5,
+                     CIDetectorMaxFeatureCount:10,
                       CIDetectorNumberOfAngles:11] as [String : Any]
         
         // setup a face detector in swift
@@ -52,8 +52,6 @@ class VideoModel: NSObject {
         
         cameraView = view
         
-        self.setupFilters()
-        
         self.videoManager.setCameraPosition(position: .front)
         self.videoManager.setProcessingBlock(newProcessBlock: self.processImage)
         
@@ -62,35 +60,79 @@ class VideoModel: NSObject {
         }
         
     }
-    
-    //MARK: Setup filtering
-    private func setupFilters(){
-        filters = []
         
-        let filterPinch = CIFilter(name:"CIBumpDistortion")!
-        filterPinch.setValue(-0.5, forKey: "inputScale")
-        filterPinch.setValue(75, forKey: "inputRadius")
-        filters.append(filterPinch)
-    }
-    
-    //MARK: Apply filters and apply feature detectors
-    private func applyFiltersToFaces(inputImage:CIImage,features:[CIFaceFeature])->CIImage{
+    // MARK: Apply Filters, Feature Detectors
+    private func applyFiltersToFaces(inputImage:CIImage, features:[CIFaceFeature]) -> CIImage{
+        
+        // Initialize Image, Face Center, Radius
         var retImage = inputImage
-        var filterCenter = CGPoint() // for saving the center of face
-        var radius = 75
+        var filterCenter = CGPoint()
+        var radius = 100
         
-        for face in features { // for each face
-            //set where to apply filter
+        // For Each Face ( < CIDetectorMaxFeatureCount:10 )
+        for face in features {
+            
+            // Set Where To Apply Filters, Radius Fix
             filterCenter.x = face.bounds.midX
             filterCenter.y = face.bounds.midY
-            radius = Int(face.bounds.width/2) // for setting the radius of the bump
+            radius = Int(face.bounds.width / 2)
             
-            for filt in filters{
-                filt.setValue(retImage, forKey: kCIInputImageKey)
-                filt.setValue(CIVector(cgPoint: filterCenter), forKey: "inputCenter")
-                filt.setValue(radius, forKey: "inputRadius")
-                //  also manipulate the radius of the filter based on face size!
-                retImage = filt.outputImage!
+            // Calculate adjusted face bounds with increased height
+            let heightIncrease: CGFloat = 300.0
+            let adjustedFaceBounds = CGRect(x: face.bounds.origin.x,
+                                            y: face.bounds.origin.y - heightIncrease / 10,
+                                            width: face.bounds.width,
+                                            height: face.bounds.height + heightIncrease)
+
+            // Highlight Entire Face With Adjusted Bounds
+            let faceHighlight = CIFilter(name: "CIHueAdjust")!
+            faceHighlight.setValue(retImage.cropped(to: adjustedFaceBounds), forKey: kCIInputImageKey)
+            faceHighlight.setValue(2.1, forKey: "inputAngle")
+            let faceImage = faceHighlight.outputImage!
+            
+            // Highlight Left Eye
+            let leftEyeHighlight = CIFilter(name: "CIRadialGradient")!
+            let leftEyeCenter = CIVector(x: face.leftEyePosition.x, y: face.leftEyePosition.y)
+            leftEyeHighlight.setValue(leftEyeCenter, forKey: "inputCenter")
+            leftEyeHighlight.setValue(10, forKey: "inputRadius0") // Inner Circle Radius
+            leftEyeHighlight.setValue(20, forKey: "inputRadius1") // Outer Circle Radius
+            leftEyeHighlight.setValue(CIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0), forKey: "inputColor0") // Solid Red Color For Inner Circle
+            leftEyeHighlight.setValue(CIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.0), forKey: "inputColor1") // Transparent Red Color For Outer Circle
+            let leftEyeImage = leftEyeHighlight.outputImage!
+
+            // Highlight Right Eye
+            let rightEyeHighlight = CIFilter(name: "CIRadialGradient")!
+            let rightEyeCenter = CIVector(x: face.rightEyePosition.x, y: face.rightEyePosition.y)
+            rightEyeHighlight.setValue(rightEyeCenter, forKey: "inputCenter")
+            rightEyeHighlight.setValue(10, forKey: "inputRadius0") // Inner Circle Radius
+            rightEyeHighlight.setValue(20, forKey: "inputRadius1") // Outer Circle Radius
+            rightEyeHighlight.setValue(CIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0), forKey: "inputColor0") // Solid Red Color for Inner Circle
+            rightEyeHighlight.setValue(CIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.0), forKey: "inputColor1") // Transparent Red Color For Outer Circle
+            let rightEyeImage = rightEyeHighlight.outputImage!
+
+            // Highlight Mouth
+            // Define a mouth rect (you may need to adjust the width and height values to best fit the mouth area)
+            let mouthWidth: CGFloat = 100.0
+            let mouthHeight: CGFloat = 100.0
+            let mouthRect = CGRect(x: face.mouthPosition.x - mouthWidth / 2,
+                                   y: face.mouthPosition.y - mouthHeight / 2,
+                                   width: mouthWidth,
+                                   height: mouthHeight)
+
+            // Apply a color adjustment to the mouth area
+            let mouthColorFilter = CIFilter(name: "CIColorControls")!
+            mouthColorFilter.setValue(retImage.cropped(to: mouthRect), forKey: kCIInputImageKey)
+            mouthColorFilter.setValue(1.2, forKey: "inputSaturation") // Increase Saturation
+            mouthColorFilter.setValue(0.2, forKey: "inputBrightness") // Boost Brightness
+            let mouthImage = mouthColorFilter.outputImage!
+            
+            // Composite the highlights over the original image
+            let featuresImages = [faceImage, leftEyeImage, rightEyeImage, mouthImage]
+            for featureImage in featuresImages {
+                let compositeFilter = CIFilter(name: "CISourceOverCompositing")!
+                compositeFilter.setValue(featureImage, forKey: kCIInputImageKey) // Top Image
+                compositeFilter.setValue(retImage, forKey: kCIInputBackgroundImageKey) // Background
+                retImage = compositeFilter.outputImage!
             }
             
             if face.hasSmile {
